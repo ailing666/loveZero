df262e4ed33110713616c5acb3e69c8ebba3fc31
v_llingai <1120877019@qq.com>
2022-04-24 19:03:26 +0800

    注册接口

--- 项目/Node+koa2/src/controller/user.controller.js
+++ 项目/Node+koa2/src/controller/user.controller.js
@@ -6,7 +6,14 @@ class UserController {
     // 操作数据库
     const res = await createUser(user_name, password);
     // 返回结果
-    ctx.body = res;
+    ctx.body = {
+      code: 0,
+      message: '用户注册成功',
+      result: {
+        id: res.id,
+        user_name: res.user_name,
+      },
+    };
   }
 
   async login(ctx, next) {
--- 项目/Node+koa2/src/service/user.service.js
+++ 项目/Node+koa2/src/service/user.service.js
@@ -1,7 +1,9 @@
+const User = require('../model/user.model');
 class UserService {
   async createUser(user_name, password) {
-    // todo: 写入数据库
-    return '写入数据成功';
+    // 插入数据
+    const res = await User.create({ user_name, password });
+    return res.dataValues;
   }
 }
 
