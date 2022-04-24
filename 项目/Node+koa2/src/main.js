37e344ae5874ae74d7f747aa5815d316a2cf944a
v_llingai <1120877019@qq.com>
2022-04-24 17:01:51 +0800

    koa-router

--- 项目/Node+koa2/src/main.js
+++ 项目/Node+koa2/src/main.js
@@ -1,13 +1,12 @@
 const Koa = require('koa');
 
 const { APP_PORT } = require('./config/config.default');
-
+// 导入路由
+const useRouter = require('./router/user');
 const app = new Koa();
 
-// 中间件
-app.use((ctx, next) => {
-  ctx.body = 'hello world';
-});
+// 中间件：注册路由
+app.use(useRouter.routes());
 
 // 启动
 app.listen(APP_PORT, () => {
