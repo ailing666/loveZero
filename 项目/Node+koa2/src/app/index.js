de6d6bec0dae18fa1310c88b1608a4611e284561
v_llingai <1120877019@qq.com>
2022-04-24 18:04:48 +0800

    拆分service

--- 项目/Node+koa2/src/app/index.js
+++ 项目/Node+koa2/src/app/index.js
@@ -1,9 +1,16 @@
 // koa相关
 const Koa = require('koa');
+
+// koa-body
+const KoaBody = require('koa-body');
+
 // 导入路由
 const useRouter = require('../router/user.router');
 
 const app = new Koa();
+
+// koa-body在其余中间件之前
+app.use(KoaBody());
 
 // 中间件：注册路由
 app.use(useRouter.routes());
--- 项目/Node+koa2/src/controller/user.controller.js
+++ 项目/Node+koa2/src/controller/user.controller.js
@@ -1,6 +1,12 @@
+const { createUser } = require('../service/user.service');
 class UserController {
   async register(ctx, next) {
-    ctx.body = '用户注册成功';
+    // 获取数据
+    const { user_name, password } = ctx.request.body;
+    // 操作数据库
+    const res = await createUser(user_name, password);
+    // 返回结果
+    ctx.body = res;
   }
 
   async login(ctx, next) {
