a7d9d1fdfb70b4108bb10c436cfc228bf7a5d00e
v_llingai <1120877019@qq.com>
2022-04-25 14:42:05 +0800

    错误处理

--- 项目/Node+koa2/src/service/user.service.js
+++ 项目/Node+koa2/src/service/user.service.js
@@ -1,9 +1,23 @@
 const User = require('../model/user.model');
 class UserService {
+  // 创建用户
   async createUser(user_name, password) {
     // 插入数据
     const res = await User.create({ user_name, password });
     return res.dataValues;
+  }
+
+  // 获取用户信息
+  async getUerInfo({ ...args }) {
+    const whereOpt = { ...args };
+    const res = await User.findOne({
+      // 查询的字段
+      attributes: ['id', 'user_name', 'password', 'is_admin'],
+      // 传入的查询的条件
+      where: whereOpt,
+    });
+    // 如果存在就返回该值，否则就返回null
+    return res ? res.dataValues : null;
   }
 }
 
