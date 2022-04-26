0fe5e4bd5a379ad1d6848406e3280837ece8d3fe
v_llingai <1120877019@qq.com>
2022-04-26 10:31:02 +0800

    service添加trycatch

--- 项目/Node+koa2/src/service/user.service.js
+++ 项目/Node+koa2/src/service/user.service.js
@@ -1,23 +1,33 @@
 const User = require('../model/user.model');
+const { userRegisterError } = require('../constant/err.type');
 class UserService {
   // 创建用户
   async createUser(user_name, password) {
-    // 插入数据
-    const res = await User.create({ user_name, password });
-    return res.dataValues;
+    try {
+      const res = await User.create({ user_name, password });
+      return res.dataValues;
+    } catch (err) {
+      ctx.app.emit('error', userRegisterError, ctx);
+      return;
+    }
   }
 
   // 获取用户信息
   async getUerInfo({ ...args }) {
     const whereOpt = { ...args };
-    const res = await User.findOne({
-      // 查询的字段
-      attributes: ['id', 'user_name', 'password', 'is_admin'],
-      // 传入的查询的条件
-      where: whereOpt,
-    });
-    // 如果存在就返回该值，否则就返回null
-    return res ? res.dataValues : null;
+    try {
+      const res = await User.findOne({
+        // 查询的字段
+        attributes: ['id', 'user_name', 'password', 'is_admin'],
+        // 传入的查询的条件
+        where: whereOpt,
+      });
+      // 如果存在就返回该值，否则就返回null
+      return res ? res.dataValues : null;
+    } catch (err) {
+      ctx.app.emit('error', userRegisterError, ctx);
+      return;
+    }
   }
 }
 
