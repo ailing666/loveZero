9c538dad1478b1da10ee0c7eb5c7d8933915c2de
v_llingai <1120877019@qq.com>
2022-04-26 12:30:40 +0800

    添加trycatch

--- 项目/Node+koa2/src/controller/user.controller.js
+++ 项目/Node+koa2/src/controller/user.controller.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { createUser, getUerInfo } = require('../service/user.service');
-const { userRegisterError } = require('../constant/err.type');
+const { userRegisterError, userLoginError } = require('../constant/err.type');
 const { JWT_SECRET } = require('../config/config.default');
 class UserController {
   // 注册接口
@@ -20,7 +20,7 @@ class UserController {
           user_name: res.user_name,
         },
       };
-    } catch (err) {
+    } catch {
       ctx.app.emit('error', userRegisterError, ctx);
       return;
     }
@@ -29,16 +29,21 @@ class UserController {
   // 登录接口
   async login(ctx, next) {
     const { user_name } = ctx.request.body;
-    // 通过 user_name 查询,生成token不包含password，所以返回的数据要剔除password
-    const { password, ...res } = await getUerInfo({ user_name });
-    ctx.body = {
-      code: 0,
-      message: '登录成功',
-      result: {
-        // 生成token：字段{id,user_name,is_admain} ,私钥, 过期时间
-        token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' }),
-      },
-    };
+    try {
+      // 通过 user_name 查询,生成token不包含password，所以返回的数据要剔除password
+      const { password, ...res } = await getUerInfo({ user_name });
+      ctx.body = {
+        code: 0,
+        message: '登录成功',
+        result: {
+          // 生成token：字段{id,user_name,is_admain} ,私钥, 过期时间
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' }),
+        },
+      };
+    } catch {
+      ctx.app.emit('error', userLoginError, ctx);
+      return;
+    }
   }
 }
 
