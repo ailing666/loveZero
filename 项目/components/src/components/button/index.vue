180c50643e045968a61c4361d9e62f4b91c0bd60
v_llingai <1120877019@qq.com>
2022-07-21 11:27:39 +0800

    feat:项目初始化

---
+++ 项目/components/src/components/button/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <button class="a-button" :class="[theme]">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AButton",
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    theme() {
+      return this.type ? `a-button-${this.type}` : "";
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.a-button {
+  padding: 0 20px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #dcdfe6;
+  border-radius: 4px;
+  background-color: #fff;
+  font-size: 14px;
+  color: #606266;
+  height: 40px;
+}
+.a-button[disabled] {
+  opacity: 0.7;
+  cursor: not-allowed;
+}
+.a-button-primary {
+  background-color: #409eff;
+  border-color: #409eff;
+  color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #409eff;
+  }
+}
+.a-button-success {
+  background-color: #00d100;
+  border-color: #00d100;
+  color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #00d100;
+  }
+}
+.a-button-wraning {
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #e6a23c;
+  }
+}
+.a-button-danger {
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #f56c6c;
+  }
+}
+/** 圆角 */
+.is-round {
+  border-radius: 100px;
+}
+</style>
---
+++ 项目/components/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a-button>默认ffff按钮</a-button>
+    <a-button type="primary">primary</a-button>
+    <a-button type="success">success</a-button>
+    <a-button type="wraning">wraning</a-button>
+    <a-button type="danger">danger</a-button>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: {
+    "a-button": () => import("@/components/button"),
+  },
+};
+</script>
