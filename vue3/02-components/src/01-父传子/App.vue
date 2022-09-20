31a91323c76c21cdc78468afcb4adb9eb965eee2
v_llingai <1120877019@qq.com>
2022-09-20 18:06:47 +0800

    父传子

---
+++ vue3/02-components/src/01-父传子/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Son abc="666" name="loveZero" :age="18" />
+</template>
+
+<script>
+import Son from './components/Son.vue';
+
+export default {
+  name: 'App',
+  components: { Son },
+};
+</script>
---
+++ vue3/02-components/src/01-父传子/components/Son.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>姓名: {{ name }}</h2>
+  <h2>年龄: {{ age }}</h2>
+  <!-- 绑定非props值 -->
+  <div v-bind="$attrs"></div>
+</template>
+
+<script>
+export default {
+  // 接受父组件传来的参数
+  props: {
+    name: {
+      type: String,
+      default: '默认值',
+    },
+    age: {
+      type: Number,
+      // 必填
+      required: true,
+      default: 0,
+    },
+    hobbies: {
+      type: Array,
+      // 数组和对象默认值需要用函数返回出来
+      default: () => ['篮球', 'rap', '唱跳'],
+    },
+    info: {
+      type: Object,
+      // 数组和对象默认值需要用函数返回出来
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style scoped></style>
