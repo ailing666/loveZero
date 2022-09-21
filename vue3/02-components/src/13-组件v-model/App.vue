46ef056ba2306d4a345ac6d910568d187f27d04e
v_llingai <1120877019@qq.com>
2022-09-21 18:03:53 +0800

    v-modle

---
+++ vue3/02-components/src/13-组件v-model/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <counter v-model="appCounter"></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue';
+
+export default {
+  components: {
+    Counter,
+  },
+  data() {
+    return {
+      appCounter: 100,
+    };
+  },
+};
+</script>
--- vue3/02-components/src/main.js
+++ vue3/02-components/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import mitt from "mitt"
-import App from './12-Keep-Alive/App.vue'
+import App from './13-组件v-model/App.vue'
 
 const app = createApp(App)
 app.config.globalProperties.$mybus = mitt()
