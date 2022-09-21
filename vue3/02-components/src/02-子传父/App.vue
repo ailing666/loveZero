e3f55454293e0273bbe2a13bc3351209e7d294db
v_llingai <1120877019@qq.com>
2022-09-21 10:28:43 +0800

    子传父

---
+++ vue3/02-components/src/02-子传父/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>当前计数: {{ counter }}</h2>
+  <Son @countAdd="addBtnClick" />
+</template>
+
+<script>
+import Son from './components/Son.vue';
+
+export default {
+  name: 'App',
+  components: { Son },
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addBtnClick(val) {
+      this.counter += val
+    },
+  },
+};
+</script>
---
+++ vue3/02-components/src/02-子传父/components/Son.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="btnClick(5)">+5</button>
+  <button @click="btnClick(10)">+10</button>
+</template>
+
+<script>
+export default {
+  // 如果使用了emit，尽量把自定义内容放到emits后面的数组中
+  emits: ['countAdd'],
+  emits: {
+    // 校验失败不影响触发，但是会在控制台警告 warning
+    countAdd: function (count) {
+      if (count === 5) {
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {
+    btnClick(val) {
+      // 触发父组件 countAdd 方法，并传入参数
+      this.$emit('countAdd', val);
+    },
+  },
+};
+</script>
