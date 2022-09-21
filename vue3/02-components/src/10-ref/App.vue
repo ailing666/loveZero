14daabe51cfd99444ffb543ba8a57f4ff2448a0e
v_llingai <1120877019@qq.com>
2022-09-21 16:29:31 +0800

    ref

---
+++ vue3/02-components/src/10-ref/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <h2 ref="title" class="title">{{ message }}</h2>
+    <banner ref="banner" />
+  </div>
+</template>
+
+<script>
+import Banner from './Banner.vue';
+export default {
+  components: {
+    Banner,
+  },
+
+  mounted() {
+    // 获取DOM元素
+    console.log('h2', this.$refs.title);
+
+    // 获取banner组件: 组件实例
+    console.log('banner', this.$refs.banner);
+
+    // 在父组件中可以主动的调用子组件的对象方法
+    this.$refs.banner.bannerClick();
+
+    // 获取banner组件实例, 获取banner中的元素
+    console.log('banner.$el', this.$refs.banner.$el);
+    // 获取根组件
+    console.log('$root', this.$root);
+  },
+};
+</script>
---
+++ vue3/02-components/src/10-ref/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="banner">
+    <h2>Banner</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    bannerClick() {
+      console.log('bannerClick被调用了');
+    },
+  },
+};
+</script>
