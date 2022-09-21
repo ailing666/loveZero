e257e5eae63e3d86dde489a516632f89404cfb89
v_llingai <1120877019@qq.com>
2022-09-21 17:55:40 +0800

    keep-alive

---
+++ vue3/02-components/src/12-Keep-Alive/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>About组件</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    // name值要与include对上
+    name: "about",
+    unmounted() {
+      console.log("about unmounted")
+    }
+  }
+</script>
+
+<style scoped>
+</style>
+
--- vue3/02-components/src/main.js
+++ vue3/02-components/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import mitt from "mitt"
-import App from './11-动态组件/App.vue'
+import App from './12-Keep-Alive/App.vue'
 
 const app = createApp(App)
 app.config.globalProperties.$mybus = mitt()
