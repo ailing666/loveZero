76d484c1b27cbb424967037eae81e7ec4ad1aead
v_llingai <1120877019@qq.com>
2022-10-08 17:50:58 +0800

    路由懒加载

--- vue3/04-vue-router/src/01-基本使用/router/index.js
+++ vue3/04-vue-router/src/01-基本使用/router/index.js
@@ -1,19 +1,24 @@
 // 导入钩子函数
-import { createRouter, createWebHashHistory,createWebHistory } from 'vue-router';
-
-// 导入子组件
-import About from '../components/About.vue';
-import Home from '../components/Home.vue';
-import User from '../components/User.vue';
-
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 // 配置路由映射
 const routes = [
   // 默认路径
   { path: '/', redirect: '/about' },
-  { path: '/home', component: Home },
-  { path: '/about', component: About },
-  { path: '/user', component: User },
+  // 路由懒加载
+  {
+    path: '/home',
+    // webpack打包注释
+    component: () => import(/* webpackChunkName: 'home'*/ '../components/About.vue'),
+  },
+  {
+    path: '/about',
+    component: () => import(/* webpackChunkName: 'about'*/ '../components/Home.vue'),
+  },
+  {
+    path: '/user',
+    component: () => import(/* webpackChunkName: 'user'*/ '../components/User.vue'),
+  },
 ];
 
 // 调用 createRouter 函数，传入配置，并指定路由模式
@@ -24,4 +29,4 @@ const router = createRouter({
 });
 
 // 导出
-export default router
+export default router;
