24f0c9ea76ef850bb0d08307ee341e8c7edb7d2e
v_llingai <1120877019@qq.com>
2022-10-21 17:17:52 +0800

    detail-info

--- 项目/vue3-trip/src/views/detail/index.vue
+++ 项目/vue3-trip/src/views/detail/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="onClickLeft" />
-    <detail-swipe :swipeData="mainPart?.topModule?.housePicture?.housePics"></detail-swipe>
+
+    <div class="main" v-if="mainPart" v-memo="[mainPart]">
+      <detail-swipe :swipeData="mainPart?.topModule?.housePicture?.housePics"></detail-swipe>
+      <detail-infos name="描述" :topInfos="mainPart.topModule" />
+    </div>
   </div>
 </template>
 
@@ -9,7 +13,8 @@
 import { ref, computed } from 'vue';
 import { getDetailInfos } from '@/services';
 import { useRoute, useRouter } from 'vue-router';
-import detailSwipe from './components/detail-swipe.vue';
+import DetailSwipe from './components/detail-swipe.vue';
+import DetailInfos from './components/detail-infos.vue';
 const router = useRouter();
 const route = useRoute();
 const houseId = route.params.id;
