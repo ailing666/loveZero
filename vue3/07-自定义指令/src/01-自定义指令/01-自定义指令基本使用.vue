4f197cd555d66a6e64a26f79a1074033dfed47c3
v_llingai <1120877019@qq.com>
2022-11-02 11:38:00 +0800

    指令

---
+++ vue3/07-自定义指令/src/01-自定义指令/01-自定义指令基本使用.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <input type="text" v-focus>
+  </div>
+</template>
+
+<script setup>
+
+const vFocus = {
+  // 生命周期的函数(自定义指令)
+  mounted(el) {
+    // console.log("v-focus应用的元素被挂载了", el)
+    el?.focus()
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
