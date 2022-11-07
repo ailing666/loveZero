200947ca80c7ae66629cbb42360db58745c4434b
v_llingai <1120877019@qq.com>
2022-11-07 11:12:54 +0800

    内含子组件

---
+++ vue3/07-自定义指令/src/02-内置组件/teleport.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 将组件挂载到body中 -->
+  <teleport to="body">
+    <hello-world />
+  </teleport>
+  <!-- 将组件挂载到index.html中abc上 -->
+  <teleport to="#abc">
+    <hello-world />
+  </teleport>
+</template>
+
+<script setup>
+import HelloWorld from './HelloWorld.vue';
+</script>
+
+
