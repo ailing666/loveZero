8ed321a13245c7d6681629316a57f829fe1ca320
v_llingai <1120877019@qq.com>
2022-11-08 16:02:43 +0800

    transition

--- vue3/07-自定义指令/src/App.vue
+++ vue3/07-自定义指令/src/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="app">
-    <h2>app</h2>
+    <div>
+      <button @click="isShow = !isShow">切换</button>
+    </div>
+
+    <transition name="abc">
+      <h2 v-if="isShow">哈哈哈哈</h2>
+    </transition>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const isShow = ref(false)
 
 </script>
 
 <style scoped>
+
+h2 {
+  display: inline-block;
+}
+
+.abc-enter-from,
+.abc-leave-to {
+  opacity: 0;
+  transform: scale(0.6);
+}
+
+.abc-enter-to,
+.abc-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.abc-enter-active,
+.abc-leave-active {
+  transition: all 2s ease;
+}
 
 </style>
