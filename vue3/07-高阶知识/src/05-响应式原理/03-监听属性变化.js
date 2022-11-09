625089ea28c995d9fa124fb363a1a6c110696246
v_llingai <1120877019@qq.com>
2022-11-09 17:15:16 +0800

    defineProperty监听

---
+++ vue3/07-高阶知识/src/05-响应式原理/03-监听属性变化.js
@@ -0,0 +1,67 @@
+class Depend {
+  constructor() {
+    this.reactiveFns = [];
+  }
+
+  addDepend(fn) {
+    if (fn) {
+      this.reactiveFns.push(fn);
+    }
+  }
+
+  notify() {
+    this.reactiveFns.forEach(fn => {
+      fn();
+    });
+  }
+}
+
+const obj = {
+  name: 'abc',
+  age: 18,
+};
+
+// 设置一个专门执行响应式函数的一个函数
+const dep = new Depend();
+function watchFn(fn) {
+  // 将函数加入reactiveFns中
+  dep.addDepend(fn);
+  fn();
+}
+
+// 监听属性变化
+Object.keys(obj).forEach(key => {
+  let value = obj[key]
+
+  Object.defineProperty(obj, key, {
+    // 设置值时通知监听
+    set: function(newValue) {
+      value = newValue
+      dep.notify()
+    },
+    get: function() {
+      return value
+    }
+  })
+})
+
+
+watchFn(function foo() {
+  console.log('foo function');
+  console.log('foo:', obj.name);
+  console.log('foo', obj.age);
+});
+
+watchFn(function bar() {
+  console.log('bar function');
+  console.log('bar:', obj.name + ' hello');
+  console.log('bar:', obj.age + 10);
+});
+
+// 修改obj的属性
+console.log('name发生变化-----------------------');
+obj.name = 'cba';
+// 通知执行
+
+console.log('age发生变化-----------------------');
+obj.age = 33;
