e78fd9187fdad2b7eabb18e4f8e5eab2d3641622
v_llingai <1120877019@qq.com>
2022-11-09 10:40:30 +0800

    动画组

---
+++ vue3/07-高阶知识/src/04-动画/04-动画组.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="app">
+    <button @click="addNumber">添加数字</button>
+    <button @click="removeNumber">删除数字</button>
+    
+    <transition-group tag="div" name="abc">
+      <template v-for="item in nums" :key="item">
+        <span>{{ item }}</span>
+      </template>
+    </transition-group>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const nums = ref([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+const addNumber = () => {
+  nums.value.splice(randomIndex(), 0, nums.value.length)
+}
+
+const removeNumber = () => {
+  nums.value.splice(randomIndex(), 1)
+}
+
+const randomIndex = () => {
+  return Math.floor(Math.random() * nums.value.length)
+}
+
+</script>
+
+<style scoped>
+
+span {
+  margin-right: 10px;
+  display: inline-block;
+}
+
+.abc-enter-from,
+.abc-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.abc-enter-to,
+.abc-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.abc-enter-active,
+.abc-leave-active {
+  transition: all 1s ease;
+}
+
+.abc-leave-active {
+  position: absolute;
+}
+
+
+/* 针对其他移动的阶段需要的动画 */
+.abc-move {
+  transition: all 1s ease;
+}
+
+</style>
--- vue3/07-高阶知识/src/App.vue
+++ vue3/07-高阶知识/src/App.vue
@@ -1,60 +1,67 @@
 <template>
   <div class="app">
-    <div>
-      <button @click="isShow = !isShow">切换</button>
-    </div>
-
-    <transition name="abc" :duration="{ enter: 800, leave: 1000 }">
-      <h2 v-if="isShow">哈哈哈哈哈哈</h2>
-    </transition>
+    <button @click="addNumber">添加数字</button>
+    <button @click="removeNumber">删除数字</button>
+    
+    <transition-group tag="div" name="abc">
+      <template v-for="item in nums" :key="item">
+        <span>{{ item }}</span>
+      </template>
+    </transition-group>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
-const isShow = ref(false);
+const nums = ref([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+const addNumber = () => {
+  nums.value.splice(randomIndex(), 0, nums.value.length)
+}
+
+const removeNumber = () => {
+  nums.value.splice(randomIndex(), 1)
+}
+
+const randomIndex = () => {
+  return Math.floor(Math.random() * nums.value.length)
+}
+
 </script>
 
 <style scoped>
-h2 {
+
+span {
+  margin-right: 10px;
   display: inline-block;
 }
 
-.abc-enter-active {
-  animation: abcAnim 2s ease;
+.abc-enter-from,
+.abc-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.abc-enter-to,
+.abc-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.abc-enter-active,
+.abc-leave-active {
+  transition: all 1s ease;
 }
 
 .abc-leave-active {
-  animation: abcAnim 2s ease reverse;
+  position: absolute;
 }
 
-@keyframes abcAnim {
-  0% {
-    transform: scale(0);
-    opacity: 0;
-  }
 
-  50% {
-    transform: scale(1.2);
-    opacity: 0.5;
-  }
-
-  100% {
-    transform: scale(1);
-    opacity: 1;
-  }
+/* 针对其他移动的阶段需要的动画 */
+.abc-move {
+  transition: all 1s ease;
 }
 
-@keyframes abcLeaveAnim {
-  0% {
-    transform: translateX(0);
-    opacity: 1;
-  }
-
-  100% {
-    transform: translateX(-500px);
-    opacity: 0;
-  }
-}
 </style>
