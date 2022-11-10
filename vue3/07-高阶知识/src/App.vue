ea5e67c202854d8923a8a89594d8368556270bdf
v_llingai <1120877019@qq.com>
2022-11-10 16:22:43 +0800

    自动收集依赖

--- vue3/07-高阶知识/src/App.vue
+++ vue3/07-高阶知识/src/App.vue
@@ -23,12 +23,33 @@ const obj = {
 };
 
 // 设置一个专门执行响应式函数的一个函数
-const dep = new Depend();
+let reactiveFn = null
 function watchFn(fn) {
-  // 将函数加入reactiveFns中
-  dep.addDepend(fn);
-  fn();
+  reactiveFn = fn
+  fn()
+  reactiveFn = null
 }
+
+// 负责通过obj的key获取对应的Depend对象
+const objMap = new WeakMap()
+function getDepend(obj, key) {
+  // 根据对象obj, 找到对应的map对象
+  let map = objMap.get(obj)
+  if (!map) {
+    map = new Map()
+    objMap.set(obj, map)
+  }
+
+  // 根据key, 找到对应的depend对象
+  let dep = map.get(key)
+  if (!dep) {
+    dep = new Depend()
+    map.set(key, dep)
+  }
+
+  return dep
+}
+
 
 // 监听属性变化
 Object.keys(obj).forEach(key => {
@@ -38,9 +59,15 @@ Object.keys(obj).forEach(key => {
     // 设置值时通知监听
     set: function(newValue) {
       value = newValue
+      // 根据key，找到对应的dep
+      const dep = getDepend(obj, key)
       dep.notify()
     },
     get: function() {
+      // 根据key，找到对应的dep
+      const dep = getDepend(obj, key)
+      // 将依赖了属性的函数，添加到reactiveFn中
+      dep.addDepend(reactiveFn)
       return value
     }
   })
@@ -55,7 +82,6 @@ watchFn(function foo() {
 
 watchFn(function bar() {
   console.log('bar function');
-  console.log('bar:', obj.name + ' hello');
   console.log('bar:', obj.age + 10);
 });
 
@@ -66,4 +92,5 @@ obj.name = 'cba';
 
 console.log('age发生变化-----------------------');
 obj.age = 33;
+
 </script>
