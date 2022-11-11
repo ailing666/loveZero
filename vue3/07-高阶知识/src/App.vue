568930f1edcd9f6f0baabf0d6f04c7681d824d4d
v_llingai <1120877019@qq.com>
2022-11-11 11:21:30 +0800

    封装函数

--- vue3/07-高阶知识/src/App.vue
+++ vue3/07-高阶知识/src/App.vue
@@ -6,7 +6,7 @@ class Depend {
 
   depend() {
     if (reactiveFn) {
-      this.reactiveFns.add(reactiveFn)
+      this.reactiveFns.add(reactiveFn);
     }
   }
 
@@ -17,62 +17,67 @@ class Depend {
   }
 }
 
-const obj = {
-  name: 'abc',
-  age: 18,
-};
-
 // 设置一个专门执行响应式函数的一个函数
-let reactiveFn = null
+let reactiveFn = null;
 function watchFn(fn) {
-  reactiveFn = fn
-  fn()
-  reactiveFn = null
+  reactiveFn = fn;
+  fn();
+  reactiveFn = null;
 }
 
 // 负责通过obj的key获取对应的Depend对象
-const objMap = new WeakMap()
+const objMap = new WeakMap();
 function getDepend(obj, key) {
   // 根据对象obj, 找到对应的map对象
-  let map = objMap.get(obj)
+  let map = objMap.get(obj);
   if (!map) {
-    map = new Map()
-    objMap.set(obj, map)
+    map = new Map();
+    objMap.set(obj, map);
   }
 
   // 根据key, 找到对应的depend对象
-  let dep = map.get(key)
+  let dep = map.get(key);
   if (!dep) {
-    dep = new Depend()
-    map.set(key, dep)
+    dep = new Depend();
+    map.set(key, dep);
   }
 
-  return dep
+  return dep;
 }
 
+/**
+ *
+ * @param {要监听的对象} obj
+ */
+function reactive(obj) {
+  // 监听属性变化
+  Object.keys(obj).forEach(key => {
+    let value = obj[key];
 
-// 监听属性变化
-Object.keys(obj).forEach(key => {
-  let value = obj[key]
+    Object.defineProperty(obj, key, {
+      // 设置值时通知监听
+      set: function (newValue) {
+        value = newValue;
+        // 根据key，找到对应的dep
+        const dep = getDepend(obj, key);
+        dep.notify();
+      },
+      get: function () {
+        // 根据key，找到对应的dep
+        const dep = getDepend(obj, key);
+        // 将依赖了属性的函数，添加到reactiveFn中
+        dep.depend();
+        return value;
+      },
+    });
+  });
+  return obj;
+}
 
-  Object.defineProperty(obj, key, {
-    // 设置值时通知监听
-    set: function(newValue) {
-      value = newValue
-      // 根据key，找到对应的dep
-      const dep = getDepend(obj, key)
-      dep.notify()
-    },
-    get: function() {
-      // 根据key，找到对应的dep
-      const dep = getDepend(obj, key)
-      // 将依赖了属性的函数，添加到reactiveFn中
-      dep.depend()
-      return value
-    }
-  })
-})
-
+const obj = reactive({
+  name: 'abc',
+  age: 18,
+});
 
 watchFn(function foo() {
   console.log('foo function');
@@ -93,5 +98,15 @@ obj.name = 'cba';
 
 console.log('age发生变化-----------------------');
 obj.age = 33;
+const user = reactive({
+  nickname: 'abc',
+  level: 100,
+});
 
+watchFn(function () {
+  console.log('nickname:', user.nickname);
+  console.log('level:', user.level);
+});
+console.log('nickname-----------------------');
+user.nickname = 'cba';
 </script>
