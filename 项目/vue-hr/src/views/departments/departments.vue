3d121f6f083557f18469030fa00187401bccb73a
loveZero <1120877019@qq.com>
2023-02-19 16:29:09 +0800

    树结构

--- 项目/vue-hr/src/views/departments/departments.vue
+++ 项目/vue-hr/src/views/departments/departments.vue
@@ -2,8 +2,87 @@
   <div class="department-container">
     <div class="app-container">
       <el-card>
-        department 组织架构
+        <!-- 用一个行列布局 -->
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
+          <el-col :span="20">
+            <svg-icon icon-class="home" /><span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <!-- 下拉菜单 element -->
+              <el-col>
+                <el-dropdown>
+                  <span>
+                    操作<i class="el-icon-arrow-down" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-tree
+          :data="list"
+          default-expand-all
+          node-key="id"
+        >
+          <template v-slot="{ node, data }">
+            <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%;">
+              <el-col :span="20">
+                <span>{{ data.name }}</span>
+              </el-col>
+              <el-col :span="4">
+                <el-row type="flex" justify="end">
+                  <!-- 两个内容 -->
+                  <el-col>负责人</el-col>
+                  <!-- 下拉菜单 element -->
+                  <el-col>
+                    <el-dropdown>
+                      <span>
+                        操作<i class="el-icon-arrow-down" />
+                      </span>
+                      <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>添加子部门</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </el-dropdown>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
+
+<script>
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils/index'
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getDepartmentsList()
+  },
+  methods: {
+    async getDepartmentsList() {
+      const res = await getDepartments()
+      res.data.depts.shift()
+      this.list = tranListToTreeData(res.data.depts)
+      console.log('this.list: ', this.list)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
