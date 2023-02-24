38df3d127f644aa7f49616874da0fd610ea1644c
loveZero <1120877019@qq.com>
2023-02-24 22:12:44 +0800

    propsType和defaultProps

--- react01/react-code/src/02_组件通信/父传子/Main.jsx
+++ react01/react-code/src/02_组件通信/父传子/Main.jsx
@@ -9,23 +9,29 @@ export class Main extends Component {
 		this.state = {
 			keywordsList: [],
 			recommendList: [],
+			returnCode: '',
+			success: false,
 		}
 	}
 
 	componentDidMount() {
 		axios.get('http://123.207.32.32:8000/home/multidata').then(res => {
+			console.log('res.data.data', res.data)
 			this.setState({
 				keywordsList: res.data.data.keywords.list,
 				recommendList: res.data.data.recommend.list,
+				returnCode: res.data.returnCode,
+				success: res.data.success,
 			})
 		})
 	}
 	render() {
-		const { keywordsList, recommendList } = this.state
+		const { keywordsList, recommendList, returnCode, success } = this.state
+		console.log('returnCode', returnCode)
 		return (
 			<>
 				<div>Main</div>
-				<MainItem keywordsList={keywordsList} />
+				<MainItem keywordsList={keywordsList} returnCode={returnCode} success={success} />
 				<MainList recommendList={recommendList} />
 			</>
 		)
--- react01/react-code/src/02_组件通信/父传子/MainItem.jsx
+++ react01/react-code/src/02_组件通信/父传子/MainItem.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export class MainItem extends Component {
+	// 定义props类型
+	static propTypes = {
+		returnCode: PropTypes.string,
+		success: PropTypes.bool,
+	}
+	// props默认值
+	static defaultProps = {
+		returnCode: 'ok',
+		success: true,
+	}
 	render() {
 		// props可以获取父组件传递的值
-		const { keywordsList } = this.props
+		const { keywordsList, returnCode } = this.props
+		console.log('returnCode', returnCode)
 		return (
 			<>
 				<h2>MainItem</h2>
+				<p>{returnCode}</p>
 				<ul>
 					{keywordsList.map(item => {
 						return <li key={item.acm}>{item.words}</li>
