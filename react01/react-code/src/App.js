e55d08d838f297d75285725840c6249ac1f790d0
v_llingai <1120877019@qq.com>
2023-03-02 17:40:16 +0800

    context

--- react01/react-code/src/App.js
+++ react01/react-code/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react'
-import NavBar from './03-插槽/NavBar'
-import NavBar2 from './03-插槽/NavBar2'
+import Home from './04-Context/Home'
+import ThemeContext from './04-Context/context/HomeContext'
+export default class App extends Component {
+	constructor() {
+		super()
 
-export class App extends Component {
+		this.state = {
+			info: { name: 'loveZero', age: 18 },
+		}
+	}
 	render() {
-		const btn = <button>按钮2</button>
-
+		const { info } = this.state
 		return (
-			<div>
-				{/* 使用children实现插槽 */}
-				<NavBar>
-					<button>按钮</button>
-					<h2>哈哈哈</h2>
-					<i>斜体文本</i>
-				</NavBar>
-
-				{/* 使用props实现插槽 */}
-				<NavBar2 leftSlot={btn} centerSlot={<h2>呵呵呵</h2>} rightSlot={<i>斜体2</i>} />
-			</div>
+			// 使用context中的Provider，传递值。名称只能是value
+			<ThemeContext.Provider value={info}>
+				<Home>App</Home>
+			</ThemeContext.Provider>
 		)
 	}
 }
-
-export default App
