25464c04daad6562a34a9b6db41fec5d9a88b9e8
loveZero <1120877019@qq.com>
2023-03-03 07:31:26 +0800

    defineExpose

---
+++ vue3/demo2/src/defineExpose/Form.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const validate = () => {
+	console.log('表单校验方法')
+}
+const formClick = () => {
+	count.value++
+}
+
+// 暴露属性给外部组件使用
+defineExpose({ count, validate })
+</script>
+
+<template>
+	<h3>我是Form组件{{ count }}</h3>
+	<button @click="formClick">我是Form组件</button>
+</template>
