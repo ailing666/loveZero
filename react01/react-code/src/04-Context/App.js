b9011d9e26c738df667bfcd62e2fc9d37894374b
v_llingai <1120877019@qq.com>
2023-03-03 15:07:43 +0800

    context默认值

--- react01/react-code/src/04-Context/App.js
+++ react01/react-code/src/04-Context/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Home from './04-Context/Home'
 import ThemeContext from './04-Context/context/HomeContext'
-import Theme from './04-Context/Theme'
 export default class App extends Component {
 	constructor() {
 		super()
@@ -13,13 +12,10 @@ export default class App extends Component {
 	render() {
 		const { info } = this.state
 		return (
-			<div>
-				{/* // 使用context中的Provider，传递值。名称只能是value */}
-				<ThemeContext.Provider value={info}>
-					<Home>App</Home>
-				</ThemeContext.Provider>
-				<Theme></Theme>
-			</div>
+			// 使用context中的Provider，传递值。名称只能是value
+			<ThemeContext.Provider value={info}>
+				<Home>App</Home>
+			</ThemeContext.Provider>
 		)
 	}
 }
