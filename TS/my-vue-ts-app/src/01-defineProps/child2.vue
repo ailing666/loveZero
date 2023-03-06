8611ace885c3648d681c55bcf0b7e3210b37e68f
loveZero <1120877019@qq.com>
2023-03-06 21:35:53 +0800

    defineProps

---
+++ TS/my-vue-ts-app/src/01-defineProps/child2.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { name, age = 999 } = defineProps<{
+	name: string
+	age?: number
+}>()
+</script>
+
+<template>
+	<div>child---{{ name }}---{{ age }}</div>
+</template>
---
+++ TS/my-vue-ts-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import child from './01-defineProps/child.vue'
+const name = ref('loveZero')
+</script>
+
+<template>
+	<child :name="name"></child>
+</template>
